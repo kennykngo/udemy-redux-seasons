b5d838069804cb96256632ff76b78e86ba0e7f8c
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-13 20:06:39 -0800

    added 3 conditional rendering sections

--- src/index.js
+++ src/index.js
@@ -5,21 +5,49 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // never want to do a direct assignment to state object
+    // THIS IS THE ONLY TIME WE do a direct assignment to this.state
     this.state = {
       // if we know that the value of the property of the state's object is a number
-      // we initialize it as 'null'
+      // we initialize lat to the value of 'null'
       lat: null,
+      errorMessage: "",
     };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        // STATE CAN ONLY BE UPDATED W/ setState function!
+        this.setState({ lat: pos.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
+  // render method gets called all of the time
+  //   render() {
+  //     return (
+  //       <div>
+  //         Latitude: {this.state.lat}
+  // 		<br />
+  //         Error: {this.state.errorMessage}
+  //       </div>
+  //     );
+  //   }
+
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      // success call back
-      (pos) => console.log(pos),
-      //failure callback
-      (err) => console.log(err)
-    );
-    return <div>Latitude: </div>;
+    // handles the error message section
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    }
+
+    // handles the latitude
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading! </div>;
   }
 }
 
