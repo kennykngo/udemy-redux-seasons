d81fac7a87ab480b6ebe94cfea615ebfb5b9bdaa
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-14 12:06:16 -0800

    added a loading page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -24,7 +25,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading! </div>;
+    return <Spinner />;
   }
 }
 
