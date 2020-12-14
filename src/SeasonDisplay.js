bf504692c60b68db0f33e2b69fc27349f5edde15
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-14 11:01:31 -0800

    got icons to show on screen depending on the season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,12 +10,15 @@ const getSeason = (lat, month) => {
 
 export default function SeasonDisplay(props) {
   const season = getSeason(props.lat, new Date().getMonth());
+  const icon = season === "winter" ? "snowflake" : "sun";
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>
         {season === "winter" ? "Burr, it's chilly" : "Let's hit the beach!"}
       </h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 }
