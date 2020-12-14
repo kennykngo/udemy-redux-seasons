d8f25c47ccce7f0eb5e2758c43990633451b8cb0
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-13 22:24:53 -0800

    refactored the code to remove constructor method

--- src/index.js
+++ src/index.js
@@ -2,37 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // never want to do a direct assignment to state object
-    // THIS IS THE ONLY TIME WE do a direct assignment to this.state
-    this.state = {
-      // if we know that the value of the property of the state's object is a number
-      // we initialize lat to the value of 'null'
-      lat: null,
-      errorMessage: "",
-    };
-
-    window.navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        // STATE CAN ONLY BE UPDATED W/ setState function!
-        this.setState({ lat: pos.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
-    console.log("My component was rendered to the screen");
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => this.setState({ lat: pos.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
-
-  componentDidUpdate() {
-    console.log("My component was just updated - it rerendered!");
-  }
-
   // render method gets called all of the time
   render() {
     // handles the error message section
